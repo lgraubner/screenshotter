835cd544d19e5d0094f6616d7db0a3c56b2041c8
Lars Graubner <mail@larsgraubner.de>
2021-08-14 08:22:40 +0200

    fix: shared files and dirs

--- deploy.php
+++ deploy.php
@@ -1,14 +1,45 @@
 <?php
 namespace Deployer;
 
-require 'recipe/symfony.php';
+require 'recipe/symfony4.php';
 
+set('bin_dir', 'bin');
+set('var_dir', 'var');
+
+// Project name
 set('application', 'screenshotter');
 
+// Project repository
 set('repository', 'git@github.com:lgraubner/screenshotter.git');
 
-add('shared_dirs', ['var']);
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', false);
+set('ssh_multiplexing', false);
+
+// Shared files/dirs between deploys
+add('shared_files', ['var/app.db']);
+add('shared_dirs', ['var/screenshots']);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
 
 host('168.119.227.77')
     ->user('deploy')
     ->set('deploy_path', '/var/www/{{application}}');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+// before('deploy:symlink', 'database:migrate');
+
